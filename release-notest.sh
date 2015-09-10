962b8122527729d9bdc97f6d5a4524c4c3d70f11
Anton P. Linevich <anton@linevich.com>
2015-09-10 02:14:53 -0700

    use jre6

--- release-notest.sh
+++ release-notest.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64/
+export PATH=/usr/lib/jvm/java-6-openjdk-amd64/bin:$PATH
 
 mvn -Dmaven.test.skip=true -DperformRelease clean compile jar:jar source:jar install
