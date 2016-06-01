e82b3ae2e2d080276c7daa762c09c2e7afbd9c27
Alex Zhukov <zhukov.alex@gmail.com>
2016-06-01 11:22:58 +0300

    build script

--- release-notest.sh
+++ release-notest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64/
-export PATH=/usr/lib/jvm/java-6-openjdk-amd64/bin:$PATH
+#export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64/
+#export PATH=/usr/lib/jvm/java-6-openjdk-amd64/bin:$PATH
 
-mvn -Dmaven.test.skip=true -DperformRelease clean compile jar:jar source:jar install
+mvn -Dmaven.test.skip=true -DperformRelease=true clean install
